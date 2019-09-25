a716aa089b4624ab480d91ad033adf8616de8f52
Stephane Dorre <stephane.dorre@de.bosch.com>
2019-09-25 16:42:09 +0200

    Millis functional

--- millis/millis.c
+++ millis/millis.c
@@ -3,7 +3,7 @@
 
 volatile unsigned long milliseconds;
 
-ISR(TIMER1_COMPA_vect) 
+ISR(TIMER1_COMPA_vect)
 {
 	milliseconds++;
 }
@@ -21,9 +21,13 @@ int main(void){
 	DDRB |= 1<<DDB1 | 1<<DDB0;
 
 	// PWM config
+	// Clear OC0A/OC0B on Compare Match
+	// Fast PWM
 	TCCR0A = 2<<COM0A0 | 2<<COM0B0 | 3<<WGM00;
+	// clock select clkI/O
 	TCCR0B = 0<<WGM02 | 1<<CS00;
 
+	// CTC Interrupt every 1ms
 	TCCR1 = 1<<CTC1 | 7<<CS10;
 	OCR1C = 0.001 * F_CPU/64 - 1;
 
@@ -33,6 +37,7 @@ int main(void){
 	sei();
 
 	while(1){
+		// Toggle PB0 (pin8) every second
 		if (cpt == 0) {
 			cpt = millis() + interval;
 		}
